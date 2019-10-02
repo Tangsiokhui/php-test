b665a9e5ef4cbd74c3d620617180a31db4c2228b
Tang Siok Hui <huiendy0913@gmail.com>
2019-10-03 02:44:14 +0800

    Create property&method.php

---
+++ property&method.php
@@ -0,0 +1,21 @@
+<?php
+
+class car{
+	public $name,
+	public $brand,
+	public $warna,
+	public $speedMax,
+	public $numCustomer;
+
+	public function increases speed(){
+
+	}
+
+	public function decrease speed(){
+
+	}
+
+	public function replace transmission{
+		
+	}
+}
